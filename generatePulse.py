f30ec8b8539d904007bc9466707b9dd64ecf849e
Jonah Spear <Jonahspear512@gmail.com>
2017-12-06 16:54:08 -0500

    generate Pulse works for backslash too

--- generatePulse.py
+++ generatePulse.py
@@ -38,10 +38,12 @@ def generateFwdSlash(totalTime, sampleRate):
 
 def generateBackSlash(totalTime, sampleRate):
     t = np.linspace(0, totalTime, totalTime * sampleRate)
-    freqs = np.linspace(1e6, 0, totalTime * sampleRate)  # hz
-    print freqs[-1] * t[-1]
-    print np.multiply(freqs, t)[-1]
-    return np.cos(2*np.pi*np.multiply(freqs, t))
+    freqs = np.linspace(2e6, 0, totalTime * sampleRate - 1)  # hz
+    dts = np.diff(t)
+    dphis = 2*np.pi*freqs*dts
+    phases = np.cumsum(dphis)
+    phases = np.insert(phases, 0, 0)
+    return np.cos(phases)
 
 
 def generateFwdAngleBracket(totalTime, sampleRate):
@@ -51,8 +53,9 @@ def generateFwdAngleBracket(totalTime, sampleRate):
 def generateBackAngleBracket(totalTime, sampleRate):
     pass
 
+
 fs = 5e6
-x = generatePulse(0, 0.1, fs)
+x = generatePulse(1, 0.1, fs)
 
 f, t, Sxx = signal.spectrogram(x, fs)
 plt.pcolormesh(t, f, Sxx)
