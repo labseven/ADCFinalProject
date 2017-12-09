cc636b8941d4b411fa97d4931ec0dc27d4e48e48
Adam Novotny <adam.novotny@postrock.wall>
2017-12-09 12:14:41 -0500

    Added downsample option

--- genPulses.py
+++ genPulses.py
@@ -7,16 +7,17 @@ from scipy import ndimage
 import pickle
 from numpy.fft import fftfreq, ifft, fftshift
 
-def reverseSpectrogam(filename):
-        f = misc.imread(filename, flatten=True, mode='F')
+
+def reverseSpectrogam(filename, downsample=10):
+        f = misc.imread(filename, flatten=True)
 
         im = np.zeros((f.shape[0]*2, f.shape[1]))
         im[:][:f.shape[0]] = f
         im[:][f.shape[0]:] = f[::-1]
 
-        x = np.zeros(im.shape[0]*im.shape[1], dtype=np.complex64)
+        x = np.zeros((im.shape[0]*im.shape[1]) // downsample, dtype=np.complex64)
 
-        for i, column in enumerate(im.T[::21]):
+        for i, column in enumerate(im.T[::downsample]):
             signalWindow = fftshift(ifft(column))
             # plt.subplot(2, 1, 1)
             # plt.plot(column)
@@ -38,6 +39,14 @@ def savePulse(filename, pulse):
     with open("pulses/{}.bin".format(filename), "wb") as outfile:
         pulse.tofile(outfile)
 
+def showSpectrogram(pulse, fs=320e3):
+    f, t, Sxx = signal.spectrogram(pulse, fs=fs)
+    # Divide by 1e6 because we are showing Mhz
+    plt.pcolormesh(t, f/1e6, Sxx)
+    plt.ylabel('Frequency [MHz]')
+    plt.xlabel('Time [sec]')
+    plt.show()
+
 if __name__ == '__main__':
     files = ['pulses/pacman.png', 'pulses/ghost.png']
 
@@ -47,3 +56,6 @@ if __name__ == '__main__':
 
     for i, pulse in enumerate(pulses):
         savePulse(i, pulse)
+
+    # for pulse in pulses:
+    #     showSpectrogram(pulse)
