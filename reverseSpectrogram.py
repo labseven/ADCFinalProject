723e96782f6d943ebd126b93e1893faef280cce9
Adam Novotny <adam.novotny@postrock.wall>
2017-12-09 10:52:59 -0500

    saving pacman pulse

--- reverseSpectrogram.py
+++ reverseSpectrogram.py
@@ -7,13 +7,13 @@ import pickle
 
 # Need to figure out how many samples 'tall' the image should be
 fs = 10e6
-t = np.linspace(0,.1,fs*.1)
+t = np.linspace(0,.1,fs*.2)
 s = np.cos(2*np.pi*t*2e6)
 
 fftCos = np.fft.fft(s)
 print(fftCos.shape)
-plt.plot(fftCos)
-plt.show()
+# plt.plot(fftCos)
+# plt.show()
 
 
 f = misc.imread('pacman.png', flatten=True)
@@ -23,21 +23,31 @@ im = np.zeros((f.shape[0]*2, f.shape[1]))
 im[:][:f.shape[0]] = f
 im[:][f.shape[0]:] = f[::-1]
 
-plt.imshow(im.T)
-plt.show()
+# plt.title("Image before Reverse Spectrogram")
+# plt.imshow(im.T)
+# plt.show()
 
-x = np.zeros(im.shape[0]*im.shape[1])
+x = np.zeros(im.shape[0]*im.shape[1], dtype=np.complex64)
 
 for i, column in enumerate(im.T):
     signalWindow = np.fft.ifft(column)
     x[i*im.shape[0]:(i+1)*im.shape[0]] = signalWindow
 
 x = x/max(abs(x))
+
 with open("pacmansignal.pk", "wb") as outfile:
     pickle.dump(x, outfile)
 
+with open("pacmansignal.bin", "wb") as outfile:
+    x.tofile(outfile)
+
+print("save successful")
+
 plt.plot(x.real)
 plt.plot(x.imag)
+plt.title("Signal")
+plt.xaxis("time")
+plt.yaxis("magnitude")
 plt.show()
 
 f, t, Sxx = signal.spectrogram(x, fs=fs, nperseg=f.shape[1])
