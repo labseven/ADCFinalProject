c69806c198f706ba8f8ad4b71879fd79b91e1c33
Adam Novotny <adam.novotny@postrock.wall>
2017-12-09 22:35:23 -0500

    Huffman encoding works

---
+++ huffmanEncoding.py
@@ -0,0 +1,149 @@
+from collections import Counter, deque
+
+class Tree():
+    def __init__(self, data=None, left=None, right=None, parent=None, weight=None, layer=None):
+        self.left = left
+        self.right = right
+        self.parent = parent
+        self.data = data
+        self.weight = weight
+        self.layer = layer
+
+    def __str__(self):
+        return("{}, {}".format(self.data, self.weight))
+
+def genHuffmanTree(text):
+    c = Counter(text)
+    c = sorted(c.items(), key=lambda c: c[1])[::-1]
+
+    nodes_remain = []
+    nodes_dict = dict()
+    for char in c:
+        nodes_remain.append(Tree(data=char[0], weight=char[1]))
+        nodes_dict[char[0]] = nodes_remain[-1]
+
+    for node in nodes_remain[::-1]:
+        print(node)
+
+    while len(nodes_remain) >= 2:
+        node1 = nodes_remain.pop()
+        node2 = nodes_remain.pop()
+
+        new_node = Tree(left=node1, right=node2, weight=node1.weight+node2.weight)
+        node1.parent = new_node
+        node2.parent = new_node
+        nodes_remain.append(new_node)
+        nodes_remain = sorted(nodes_remain, key=lambda node: node.weight)[::-1]
+
+    return nodes_remain[0], nodes_dict
+
+def genHuffmanFromFile(filename):
+    with open(filename, "r") as infile:
+        data=infile.read().replace('\n', '')
+
+    root, treeDict = genHuffmanTree(data)
+    huffDict = genHuffmanEncodeDict(root)
+
+    return root, huffDict
+
+
+def printTree(root):
+    nodes_remain = deque([root])
+
+    i = 0
+    while nodes_remain:
+        i += 1
+        node = nodes_remain.pop()
+
+        if node.data:
+            print(node.data, node.weight)
+        else:
+            nodes_remain.append(node.left)
+            nodes_remain.append(node.right)
+
+def decodeHuffman(root, data):
+    curNode = root
+    output = []
+    for x in data:
+        if x == 1:
+            curNode = curNode.left
+        if x == 0:
+            curNode = curNode.right
+
+        if curNode.data:
+            output.append(curNode.data)
+            curNode = root
+
+    if curNode != root:
+        print("error, bits remain")
+
+    return output
+
+def listToStr(inList):
+    string = ""
+    for x in inList:
+        string = string + str(x)
+    return string
+
+def genHuffmanEncodeDict(root):
+    huffDict = dict()
+    curNode = root
+    lastNode = root
+
+    curEncode = []
+
+    while True:
+        if curNode.data:
+            huffDict[curNode.data] = tuple(curEncode)
+            lastNode = curNode
+            curNode = curNode.parent
+            curEncode.pop()
+
+        elif curNode.right == lastNode:
+            if curNode == root:
+                break
+
+            lastNode = curNode
+            curNode = curNode.parent
+            curEncode.pop()
+
+        elif curNode.left == lastNode:
+            lastNode = curNode
+            curNode = curNode.right
+            curEncode.append(0)
+
+        else:
+            lastNode = curNode
+            curNode = curNode.left
+            curEncode.append(1)
+
+    return huffDict
+
+
+
+def encodeHuffman(treeDict, message):
+    data = []
+    for c in message:
+        for i in treeDict[c]:
+            data.append(i)
+
+    return data
+
+if __name__ == '__main__':
+    with open("english.txt", "r") as infile:
+        data=infile.read().replace('\n', '')
+
+    root, treeDict = genHuffmanTree(data)
+    huffDict = genHuffmanEncodeDict(root)
+
+    # print(huffDict)
+    # for i in huffDict:
+    #     print("{}: {}".format(i, huffDict[i]))
+
+    message = encodeHuffman(huffDict, "hi, I'm doing well")
+
+    printTree(root)
+
+    print(message)
+    decoded = decodeHuffman(root, message)
+    print(listToStr(decoded))
